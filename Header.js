0736372bb9267b57126c8595d276ddd00115da74
JVCOB1119 <jakub.piecyk02@gmail.com>
2026-01-19 20:48:41 +0100

    feat(header): add basic header component

---
+++ Header.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={{ padding: 16 }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>GeoNotes</Text>
+    </View>
+  );
+}
